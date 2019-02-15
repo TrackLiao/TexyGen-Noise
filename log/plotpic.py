d22aa479fe43001e464a73ab54361daaacd69fb0
TrackLiao <track.liao@gamil.com>
2019-02-15 08:41:52 +0000

    update graph sytle

--- log/plotpic.py
+++ log/plotpic.py
@@ -11,7 +11,7 @@ def load_data(filename, y_type):
         next(plots, None) #skip header
         for row in plots:
             xnum = round(float(row[0]), 4)
-            ynum = round(float(row[y_type]), 4)
+            ynum = round(float(row[y_type]), 10)
             x.append(xnum)
             y.append(ynum)
         return x , y
@@ -19,50 +19,52 @@ def load_data(filename, y_type):
 
 #graph for EmbSic
 x_swap, y_swap = load_data('seqgan-swap.csv', 1)
-plt.plot(x_swap,y_swap, label='swap')
+plt.plot(x_swap,y_swap, linestyle='-', marker='o' ,label='swap')
 
 x_reverse, y_reverse = load_data('seqgan-reverse.csv', 1)
-plt.plot(x_reverse,y_reverse, label='reverse')
+plt.plot(x_reverse,y_reverse, linestyle='--', marker='v' ,label='reverse')
 
 x_misword, y_misword = load_data('seqgan-misword.csv', 1)
-plt.plot(x_misword,y_misword, label='misword')
+plt.plot(x_misword,y_misword,linestyle='-.', marker='X' ,label='misword')
 
 x_wtense, y_wtense = load_data('seqgan-wtense.csv', 1)
-plt.plot(x_wtense,y_wtense, label='wtense')
+plt.plot(x_wtense,y_wtense,linestyle=':', marker='s' ,label='wtense')
 
 x_typo, y_typo = load_data('seqgan-typo.csv', 1)
-plt.plot(x_typo,y_typo, label='typo')
+plt.plot(x_typo,y_typo,linestyle='-', marker='d' ,label='typo')
 
 plt.xlabel('Epoches')
-plt.ylabel('EmbeddingSimilarity')
+plt.ylabel('Embedding Similarity')
 plt.title("Noise Type")
 plt.legend()
 
-# plt.show()
+plt.tight_layout()
+plt.show()
 plt.savefig('EmbSim.png')
 plt.close()
 
 #graph for nll-test
 x_swap, y_swap = load_data('seqgan-swap.csv', 2)
-plt.plot(x_swap,y_swap, label='swap')
+plt.plot(x_swap,y_swap, linestyle='-', marker='o' ,label='swap')
 
 x_reverse, y_reverse = load_data('seqgan-reverse.csv', 2)
-plt.plot(x_reverse,y_reverse, label='reverse')
+plt.plot(x_reverse,y_reverse, linestyle='--', marker='v' ,label='reverse')
 
 x_misword, y_misword = load_data('seqgan-misword.csv', 2)
-plt.plot(x_misword,y_misword, label='misword')
+plt.plot(x_misword,y_misword,linestyle='-.', marker='X' ,label='misword')
 
 x_wtense, y_wtense = load_data('seqgan-wtense.csv', 2)
-plt.plot(x_wtense,y_wtense, label='wtense')
+plt.plot(x_wtense,y_wtense,linestyle=':', marker='s' ,label='wtense')
 
 x_typo, y_typo = load_data('seqgan-typo.csv', 2)
-plt.plot(x_typo,y_typo, label='typo')
+plt.plot(x_typo,y_typo,linestyle='-', marker='d' ,label='typo')
 
 plt.xlabel('Epoches')
-plt.ylabel('NLL-test')
+plt.ylabel('NLL-test loss')
 plt.title("Noise Type")
 plt.legend()
 
-# plt.show()
+plt.tight_layout()
+plt.show()
 plt.savefig('NLL-test.png')
 plt.close()
